5a6881f228a89f14f24ed85cdf9db1331e13d793
iLeaf <studio_ileaf@yahoo.co.jp>
2012-09-02 19:47:41 +0900

    first commit

---
+++ SampleEvernote/studio_iLeafAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  studio_iLeafAppDelegate.h
+//  SampleEvernote
+//
+//  Created by 平杉 敦史 on 12/09/02.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface studio_iLeafAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UINavigationController *navigationController;
+
+@end
---
+++ SampleEvernote/studio_iLeafMasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  studio_iLeafMasterViewController.h
+//  SampleEvernote
+//
+//  Created by 平杉 敦史 on 12/09/02.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class studio_iLeafDetailViewController;
+
+@interface studio_iLeafMasterViewController : UITableViewController
+
+@property (strong, nonatomic) studio_iLeafDetailViewController *detailViewController;
+
+@end
